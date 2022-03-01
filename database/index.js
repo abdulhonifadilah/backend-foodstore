669a552bcf8a9b9afc4657f436723b819aa6259e
Abdul Honi Fadilah <abdulhoni.fadilah@gmail.com>
2022-03-01 08:59:17 +0700

    koneksikan ke mongo cloud

--- database/index.js
+++ database/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const { dbUser, dbName, dbPass, dbHost, dbPort } = require('../app/config');
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+mongoose.connect('mongodb+srv://admin:root@cluster0.xhm4h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 // mongodb://localhost:27017/foodstore
 // mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin
+// `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`
 db = mongoose.connection;
 
 
