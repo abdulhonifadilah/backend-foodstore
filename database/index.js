7a2590a4a7bff8c9a3d5513a9be340b5b57623a2
Abdul Honi Fadilah <abdulhoni.fadilah@gmail.com>
2022-03-02 10:12:15 +0700

    mongodb

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { dbUser, dbName, dbPass, dbHost, dbPort } = require('../app/config');
 
-mongoose.connect(process.env.MONGO_URL||'mongodb+srv://admin:root@cluster0.xhm4h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGO_URL||'mongodb+srv://admin:root@cluster0.xhm4h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true });
 // mongodb://localhost:27017/foodstore
 // mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin
 // `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`
